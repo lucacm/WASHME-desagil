df879e27ce9bb6bbdc4e29d660c5d4e8cd842a22
murilo menezes <murilolcm@al.insper.edu.br>
2020-06-03 00:10:00 -0300

    login implementado

--- app/src/main/java/br/edu/insper/al/lucacm/washme/MainActivity.java
+++ app/src/main/java/br/edu/insper/al/lucacm/washme/MainActivity.java
@@ -1,27 +1,32 @@
 package br.edu.insper.al.lucacm.washme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navBar = findViewById(R.id.bottom_navigation);
 
-        Button loginButton = findViewById(R.id.loginButton);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(navBar, navController);
 
-        loginButton.setOnClickListener((view) ->{
+        NavigationUI.setupActionBarWithNavController(this, navController);
 
-            Intent intent = new Intent(this,BottomNavigationView.class);
-
-            startActivity(intent);
-
-        });
 
     }
+
+
 }
